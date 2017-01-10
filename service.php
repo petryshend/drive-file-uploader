4e1c78adf37ff41e945003770cd3a92fd2d21bef
petryshend <petryshend@gmail.com>
2017-01-10 18:22:28 +0200

    Remove commented lines

--- service.php
+++ service.php
@@ -75,19 +75,3 @@ function expandHomeDirectory($path) {
 $client = getClient();
 $service = new Google_Service_Drive($client);
 return $service;
-
-// Print the names and IDs for up to 10 files.
-//$optParams = array(
-//  'pageSize' => 10,
-//  'fields' => 'nextPageToken, files(id, name)'
-//);
-//$results = $service->files->listFiles($optParams);
-//
-//if (count($results->getFiles()) == 0) {
-//  print "No files found.\n";
-//} else {
-//  print "Files:\n";
-//  foreach ($results->getFiles() as $file) {
-//    printf("%s (%s)\n", $file->getName(), $file->getId());
-//  }
-//}
